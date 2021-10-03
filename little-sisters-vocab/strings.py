fd9158796ac2c0778a6e3d50f20e6839078e3a60
Leonardo <lnatera27@gmail.com>
2021-10-03 14:25:10 -0500

    📖 Little sisters vocab

---
+++ little-sisters-vocab/strings.py
@@ -0,0 +1,67 @@
+def add_prefix_un(word: str) -> str:
+    """
+
+    :param word: str of a root word
+    :return:  str of root word with un prefix
+
+    This function takes `word` as a parameter and
+    returns a new word with an 'un' prefix.
+    """
+
+    return "un" + word
+
+
+def make_word_groups(vocab_words: "list[str]") -> str:
+    """
+
+    :param vocab_words: list of vocabulary words with a prefix.
+    :return: str of prefix followed by vocabulary words with
+             prefix applied, separated by ' :: '.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix  and the words with prefix applied, separated
+     by ' :: '.
+    """
+
+    string = ''
+
+    for word in vocab_words:
+        if word == vocab_words[0]:
+            string = word
+        else:
+            string += (' :: ' + vocab_words[0] + word)
+
+    return string
+
+
+def remove_suffix_ness(word: str) -> str:
+    """
+
+    :param word: str of word to remove suffix from.
+    :return: str of word with suffix removed & spelling adjusted.
+
+    This function takes in a word and returns the base word with `ness` removed.
+    """
+
+    if word[-5] == 'i':
+        return word[:-5] + 'y'
+
+    return word[:-4]
+
+
+def noun_to_verb(sentence: str, index: int) -> str:
+    """
+
+    :param sentence: str that uses the word in sentence
+    :param index:  index of the word to remove and transform
+    :return:  str word that changes the extracted adjective to a verb.
+
+    A function takes a `sentence` using the
+    vocabulary word, and the `index` of the word once that sentence
+    is split apart.  The function should return the extracted
+    adjective as a verb.
+    """
+    sentence_without_dot = sentence[:-1]
+    word_list = sentence_without_dot.split()
+
+    return word_list[index] + 'en'
